91a12f8c0ab15fe9862701bb7c2352aef08cddd0
Vivek Dogra <vivekdogra02@gmail.com>
2018-12-26 23:50:51 +0530

    Search funtionality done

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Search from '../tracks/Search';
 import Tracks from '../tracks/Tracks';
 const Index = () => {
   return (
     <React.Fragment>
+      <Search />
        <Tracks />
     </React.Fragment>
   )
--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import axios from 'axios';
-import Spinner from "../layout/Spinner";
+
 import { Link } from "react-router-dom";
 import Moment from  'react-moment';
+import Spinner from "../layout/Spinner";
+import axios from 'axios';
 
 class Lyrics extends Component {
    state = {
@@ -15,6 +16,9 @@ class Lyrics extends Component {
          this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
       .then(res => {
          console.log(res.data);
+         if(res.data.body) {
+            this.props.history.push('/');
+         }
          this.setState({lyrics: res.data.message.body.lyrics});
          return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${
             this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
@@ -52,9 +56,12 @@ class Lyrics extends Component {
                <li className="list-group-item">
                <strong>Album ID</strong>: {track.album_id}
                </li>
-               <li className="list-group-item">
-               <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
-               </li>
+                {track.primary_genres && track.primary_genres.music_genre_list.length > 0 ? (
+                          <li className="list-group-item">
+                          <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+                          </li>
+                 ) : null
+                 }
                <li className="list-group-item">
                <strong> Explicit Words </strong>: {track.explicit === 0 ? 'No' : 'Yes'}
                </li>
