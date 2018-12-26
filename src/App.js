327224e54b0995fb5e01e819711a79b9a97a7897
Vivek Dogra <vivek.dogra@nagarro.com>
2018-12-26 18:17:44 +0530

    Lyrics api added with lyrics page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+
 import {Provider} from './context';
 import "./App.css";
 class App extends Component {
@@ -15,6 +17,7 @@ class App extends Component {
             <Switch>
                <Route exact path="/" component={Index}>
                </Route>
+               <Route exact path="/lyrics/track/:id"  component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
--- src/context.js
+++ src/context.js
@@ -8,7 +8,7 @@ export class Provider extends Component {
       heading: 'Top 10 Tracks'
    };
    componentDidMount() {
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=IN&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=US&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
       .then(res => {
          this.setState({track_list: res.data.message.body.track_list})
       })
