8a5ebb798b07b012521810118f604fee71e4669a
Vivek Dogra <vivekdogra02@gmail.com>
2018-12-28 01:00:42 +0530

    Updates

---
+++ precache-manifest.6f779707c366eafdcba6e29d604b3790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bbd3c3f241158cbe70",
+    "url": "/lyrics-finder/static/js/runtime~main.e4bbd3c3.js"
+  },
+  {
+    "revision": "a9964e155ed99a429d2d",
+    "url": "/lyrics-finder/static/js/main.a9964e15.chunk.js"
+  },
+  {
+    "revision": "860697b44dbf56012862",
+    "url": "/lyrics-finder/static/js/1.860697b4.chunk.js"
+  },
+  {
+    "revision": "a9964e155ed99a429d2d",
+    "url": "/lyrics-finder/static/css/main.34a994d1.chunk.css"
+  },
+  {
+    "revision": "b54d8017a5be702dc470e05930ec3665",
+    "url": "/lyrics-finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.873022749576413ad8382310afc63481.js"
+  "/lyrics-finder/precache-manifest.6f779707c366eafdcba6e29d604b3790.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lyrics-finder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
